f2f2ec7a7855f60e32c88989f6ed0c627629102e
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2022-09-02 13:26:54 +0200

    icons themes assest
    added the icons in the assets folder then created a icons.js to export all the icons ass usable varibles.
    Added a thems.js under constants to use for the theming of the app.
    channged the tab navigator images to the icons.

--- untoldtravel/src/navigation/TabNavigator.js
+++ untoldtravel/src/navigation/TabNavigator.js
@@ -1,5 +1,6 @@
 //import liraries
 import React, { Component } from 'react';
+import { Image } from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -7,16 +8,53 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import SearchScreen from '../screens/SearchScreen';
 import FavoriteScreen from '../screens/FavoriteScreen';
+import icons from '../constants/icons';
 
 const Tab = createBottomTabNavigator();
 
 // create a component
 const TabNavigator = () => {
     return (
-        <Tab.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
-            <Tab.Screen name='Home' component={HomeScreen} />
-            <Tab.Screen name='Search' component={SearchScreen} />
-            <Tab.Screen name='Favorite' component={FavoriteScreen} />
+        <Tab.Navigator 
+            initialRouteName='Home' 
+            screenOptions={{
+                headerShown: false, tabBarShowLabel: false
+                }}
+        >
+            <Tab.Screen 
+                name='Home' 
+                component={HomeScreen} 
+                options={{
+                    tabBarIcon: () => {
+                        return (
+                            <Image source={icons.Home} resizeMode='cover' />
+                        );
+                    },
+                }}
+            />
+            <Tab.Screen 
+                name='Search' 
+                component={SearchScreen} 
+                options={{
+                    tabBarIcon: () => {
+                        return (
+                            <Image source={icons.Search} resizeMode='cover' />
+                        );
+                    },
+                }}
+            />
+            <Tab.Screen 
+                name='Favorite' 
+                component={FavoriteScreen} 
+                options={{
+                    tabBarIcon: () => {
+                        return (
+                            <Image source={icons.Favorite} resizeMode='cover' />
+                        );
+                    },
+                }}
+            />
+
         </Tab.Navigator>
     );
 };
