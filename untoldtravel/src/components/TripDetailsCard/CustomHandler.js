f7153acca62656ea981d279a2bc3bb71e2d307f8
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2022-09-03 17:44:13 +0200

    added bootom pull up thing

---
+++ untoldtravel/src/components/TripDetailsCard/CustomHandler.js
@@ -0,0 +1,13 @@
+//import liraries
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+// create a component
+const CustomHandler = () => {
+    return (
+        <View />
+    );
+};
+
+//make this component available to the app
+export default CustomHandler;
--- untoldtravel/src/components/TripDetailsCard/TripDetailsCard.js
+++ untoldtravel/src/components/TripDetailsCard/TripDetailsCard.js
@@ -1,40 +1,33 @@
 //import liraries
-import React, { Component } from 'react';
+import React, { Component, useMemo } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import {colors, sizes, spacing} from '../../constants/theme';
 import * as Animatable from 'react-native-animatable';
 import BottomSheet from '@gorhom/bottom-sheet';
+import CustomHandler from './CustomHandler';
 
 
 // create a component
 const TripDetailsCard = ({trip}) => {
+  const snapPoints = useMemo(() => ['30%', '80%']);
     return (
-      <View style={styles.card}>
-        <View>
-          <Animatable.View
-            style={styles.header}
-            animation="fadeInUp"
-            delay={500}
-            easing="ease-in-out"
-            duration={400}
-          > 
-            <Text style={styles.title}>{trip.title}</Text>
-            <Text style={styles.location}>{trip.location}</Text>
-          </Animatable.View>
-        </View>
-      </View>
+      <BottomSheet snapPoints={snapPoints} handleComponent={CustomHandler} index={0} >
+            <Animatable.View
+              style={styles.header}
+              animation="fadeInUp"
+              delay={500}
+              easing="ease-in-out"
+              duration={400}
+            > 
+              <Text style={styles.title}>{trip.title}</Text>
+              <Text style={styles.location}>{trip.location}</Text>
+            </Animatable.View>
+      </BottomSheet>
     );
   };
 
 // define your styles
 const styles = StyleSheet.create({
-    card: {
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      right: 0,
-      height: '30%',
-    },
     header: {
       paddingVertical: spacing.l,
       paddingHorizontal: spacing.l,
@@ -42,11 +35,11 @@ const styles = StyleSheet.create({
     title: {
       fontSize: sizes.title,
       fontWeight: 'bold',
-      color: colors.white,
+      color: colors.primary,
     },
     location: {
       fontSize: sizes.title,
-      color: colors.white,
+      color: colors.primary,
     },
   });
 
